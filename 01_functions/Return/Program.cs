8b3b4d61642995ab16a7d7b5ea654acab4adb095
Clouddyyyy <leveringcharlotte@gmail.com>
2025-11-30 17:19:18 +0100

    Made 02_classes task 1

--- 01_functions/Return/Program.cs
+++ 01_functions/Return/Program.cs
@@ -5,32 +5,40 @@ internal class Program
     string[] vragen = new string[]
     {
         "What 1997 N64 video game features James Bond and is named after the 1996 film?",
-        "What arcade game was called Puckman in Japan?"
+        "What arcade game was called Puckman in Japan?",
+        "Which spooky 2001 GameCube game starring Mario's brother got a reboot for Nintendo Switch in 2019?",
+        "Which video game console released in 2006 pioneered the use of motion controls in its gameplay?"
+
     };
+    
+    
     internal string GetVraag(int vraagIndex)
     {
         return vragen[vraagIndex];
-
-        string vraag0 = GetVraag(0);
-        string vraag1 = GetVraag(1);
-        string vraag2 = GetVraag(2);
-        string vraag3 = GetVraag(3);
-        string vraag4 = GetVraag(4);
     }
 
 
     static void Main(string[] args)
     {
         Program program = new Program();
-        program.Run();
+        program.vraag1();
+        program.vraag2();
+        program.vraag3();
+        program.vraag4();
+        program.vraag5();
+        program.vraag6();
+        program.vraag7();
     }
     internal void Run()
     {
-        Console.WriteLine("How long do you think you'd survive in a zombie apocalyspe?");
-        Console.WriteLine("Have you ever tried talking to a animal?");
-        Console.WriteLine("Which species would be the rudest if all animals could talk?");
-        Console.WriteLine("What would you do if you won a million dollars?");
-        Console.WriteLine("What is the worst game you played but that you liked anyway?");
+        string vraag0 = GetVraag(0);
+        string vraag1 = GetVraag(1);
+        string vraag2 = GetVraag(2);
+        string vraag3 = GetVraag(3);
+        string vraag4 = GetVraag(4);
+        string vraag5 = GetVraag(5);
+        string vraag6 = GetVraag(6);
+        string vraag7 = GetVraag(7); 
     }
 
     internal void vraag1()
@@ -76,18 +84,24 @@ internal class Program
 
         Console.WriteLine(antwoord);
     }
-    internal void vraag7()
+    internal string vraag7()
     {
         Console.WriteLine("What is the worst game you played but that you liked anyway?");
         string antwoord = Console.ReadLine();
-
+        Console.WriteLine("antwoord");
         return antwoord;
-        string antwoord7 = vraag7();
-        Console.WriteLine("antwoord7");
-    }
+    }    
     internal string GetRandomVraag()
     {
-        int random = 6;
-        return;
+       
+       Random dice = new Random(); //maak random
+        string[] Random = ["What 1997 N64 video game features James Bond and is named after the 1996 film?",
+         "What arcade game was called Puckman in Japan?", 
+         "Which spooky 2001 GameCube game starring Mario's brother got a reboot for Nintendo Switch in 2019?",
+         "Which video game console released in 2006 pioneered the use of motion controls in its gameplay?",]; //roll;
+
+        int GetRandomVraag= dice.Next(Random.Length);
+        
+        return GetVraag(GetRandomVraag); //pak vraag;
     }
 }
---
+++ 02_classes/RijFolder/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace RijFolder;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
